48572a66568f888e3acf29e66365953517709407
Zeo G <zeog14@gmail.com>
2019-10-06 21:24:57 -0500

    created abstract method letterAverage()

--- MesoAbstract.java
+++ MesoAbstract.java
@@ -7,6 +7,7 @@ public abstract class MesoAbstract
 	//Abstract method calAverage to return integer array.
 	abstract int[] calAverage();
 
-	//TODO: Create an abstract method letterAverage with return type char
+	// Abstract method letterAverage to return type char
+	abstract char[] letterAverage();
 	
 }
