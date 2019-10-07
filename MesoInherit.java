6994797d92d65ffc6bf1e7efee347f4a81006c5d
Zeo G <zeog14@gmail.com>
2019-10-06 21:41:50 -0500

    Initial commit

---
+++ MesoInherit.java
@@ -0,0 +1,25 @@
+
+public class MesoInherit extends MesoAbstract {
+	
+	//constructor: what values need to set?
+	public MesoInherit()
+	{
+		//Todo: set values
+	}
+
+	/**
+	@Override
+	*/
+	int[] calAverage() {						//Auto generated: must be implemented with PosAvg.java
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	/**
+	@Override
+	*/											//Auto generated: must be implemented with PosAvg.java
+	char[] letterAverage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
