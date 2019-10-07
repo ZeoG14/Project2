16c6317011404dc960fb7eb88c6811dacb4db21f
Zeo G <zeog14@gmail.com>
2019-10-06 23:20:48 -0500

    created toString stub

--- LetterAvg.java
+++ LetterAvg.java
@@ -1,5 +1,22 @@
 
 public class LetterAvg extends MesoInherit
 {
-
+	
+	public LetterAvg()
+	{
+		//constructor
+	}
+	
+	//from driver
+	public int numberOfStationWithLetterAvg()
+	{
+		return 0;
+	}
+	
+	//will need to return formatted string (From driver)
+	public String toString()
+	{
+			return null;
+	}
+	
 }
