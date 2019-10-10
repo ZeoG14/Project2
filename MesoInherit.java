c689752243168232f53007ec6a5ccab7a7e7d1a3
Zeo G <zeog14@gmail.com>
2019-10-10 13:06:49 -0500

    Changed getAverage() to round more efficiently

--- MesoInherit.java
+++ MesoInherit.java
@@ -4,155 +4,184 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class MesoInherit extends MesoAbstract {
-	
-	//creating list to hold all the names of the stations
+
+	/**
+	 * Array list to hold the names of all the stations
+	 */
 	protected static ArrayList<String> stations = new ArrayList<String>();
-	
-	//MesoStation object containting the stID
+
+	/**
+	 * MesoStation object containting the stID
+	 */
 	private MesoStation mesoStation;
-	
-	//default constructor
+
+	/**
+	 * default empty constructor
+	 */
 	public MesoInherit() 
 	{
-		
+		stations = new ArrayList<String>();
+		mesoStation = null;
 	}
-	
-	//constructor: what values need to set?
-	public MesoInherit(MesoStation mesoStation)throws IOException
+
+	/**
+	 * 
+	 * @param Receives mesoStation object and assigns it to the class level
+	 *                 mesoStation
+	 * @throws IOException
+	 */
+	public MesoInherit(MesoStation mesoStation) throws IOException 
 	{
 		readStations();
 		this.mesoStation = mesoStation;
 	}
 
 	/**
-	@Override
-	*/
-	int[] calAverage()			
-	{		
+	 * @Override Remove the Ceiling, Floor, and Average values in an int array
+	 */
+	int[] calAverage() 
+	{
 		int[] average = new int[3];
-		
+
 		average[0] = getCeiling();
 		average[1] = getFloor();
 		average[2] = getAverage();
-	
+
 		return average;
 	}
-	
+
 	/**
-	 * @Overide
+	 * @Overide Returns the letterAverage which is just the getAverage() casted to a
+	 *          char
 	 */
-	protected char letterAverage()
+	protected char letterAverage() 
 	{
-		
-		return (char)getAverage();
-		
+
+		return (char) getAverage();
+
 	}
-	
-	 /**
-     * Creates a list of station from the txt file. Each line of the txt represents a single station.
-     * @param filename The file to read from.
-     * @throws IOException
-     */
-	public static void readStations() throws IOException
+
+	/**
+	 * Creates a list of station from the txt file. Each line of the txt represents
+	 * a single station.
+	 * 
+	 * @param filename The file to read from.
+	 * @throws IOException
+	 */
+	public static void readStations() throws IOException 
 	{
+
 		BufferedReader br = new BufferedReader(new FileReader("Mesonet.txt"));
 
-    	String strg;
-    	// Throw out unnecessary data
-    	strg = br.readLine();
-    	strg = br.readLine();
-    	//First row containing data
-    	strg = br.readLine();
+		String strg;
+		// Throw out unnecessary data
+		strg = br.readLine();
+		strg = br.readLine();
+		strg = br.readLine();
+		// First row containing data
+		strg = br.readLine();
 
+		while (strg != null) {
 
-    	while(strg != null)
-    	{
+			strg = strg.substring(1, 5);
+			stations.add(strg);
 
-    		strg = strg.substring(1,5);
-    		stations.add(strg);
-
-    		strg = br.readLine();
-    	}
-    	br.close();
+			strg = br.readLine();
+		}
+		br.close();
 	}
-	
-	
-	//for loop to add up ascii values and get ceiling division
-	public int getCeiling()
+
+	/**
+	 * for loop to add up ascii values and get ceiling division
+	 * 
+	 * @return Returns an the sum of the ascii values divided by the length of the
+	 *         mesoStation ID and performs ceiling division
+	 */
+	public int getCeiling() 
 	{
-		//initilizing return
+		// initilizing return
 		int ceiling = 0;
-		//variable to hold the sum of all the ascii values
+		// variable to hold the sum of all the ascii values
 		double sum = 0.0;
-		//variable to hold the double division of the sum 4 to be able to cast ceiling as an int
+		// variable to hold the double division of the sum 4 to be able to cast ceiling
+		// as an int
 		double average = 0.0;
-		
-		for(int i = 0; i < mesoStation.getStID().length(); i++ )
+		// for loop to add up all the ascii values
+		for (int i = 0; i < mesoStation.getStID().length(); i++) 
 		{
 			sum += mesoStation.getStID().charAt(i);
 		}
-		
-		//getting the raw double ascii average
-		average = sum / 4;
-		//performing ceiling rounding on the ascii average and casting to int and assigning it to ceiling
-		ceiling = (int)Math.ceil(average);
-		
-		return ceiling; 
+
+		// getting the raw double ascii average
+		average = sum / mesoStation.getStID().length();
+		// performing ceiling rounding on the ascii average and casting to int and
+		// assigning it to ceiling
+		ceiling = (int) Math.ceil(average);
+
+		return ceiling;
 	}
-	
-	//for loop to add up ascii values and get floor division
-		public int getFloor()
+
+	// for loop to add up ascii values and get floor division
+	public int getFloor() 
+	{
+		// initilizing return
+		int floor = 0;
+		// variable to hold the sum of all the ascii values
+		double sum = 0.0;
+		// variable to hold the double division of the sum 4 to be able to cast ceiling
+		// as an int
+		double average = 0.0;
+		// for loop to add up all the ascii values
+		for (int i = 0; i < mesoStation.getStID().length(); i++) 
 		{
-			//initilizing return
-			int floor = 0;
-			//variable to hold the sum of all the ascii valuesd
-			double sum = 0.0;
-			//variable to hold the double division of the sum 4 to be able to cast ceiling as an int
-			double average = 0.0;
-			for(int i = 0; i < mesoStation.getStID().length(); i++ )
-			{
-				sum += mesoStation.getStID().charAt(i);
-			}
-				
-			//getting the raw double ascii average
-			average = sum / 4;
-			//performing ceiling rounding on the ascii average and casting to int and assigning it to ceiling
-			floor = (int)Math.floor(average);
-				
-			return floor; 
+			sum += mesoStation.getStID().charAt(i);
 		}
-		
-		//for loop to add up ascii values and get ceiling division
-		public int getAverage()
+
+		// getting the raw double ascii average
+		average = sum / mesoStation.getStID().length();
+		// performing floor division on the ascii average and casting to int and
+		// assigning it to ceiling
+		floor = (int) Math.floor(average);
+
+		return floor;
+	}
+
+	// for loop to add up ascii values and get ceiling division
+	public int getAverage() 
+	{
+		// initilizing return
+		int avg = 0;
+		// variable to hold the sum of all the ascii values
+		double sum = 0.0;
+		// variable to hold the double division of the sum 4 to be able to cast ceiling
+		// as an int
+		double average = 0.0;
+		// for loop to add up all the ascii values
+		for (int i = 0; i < mesoStation.getStID().length(); i++) 
 		{
-			//initilizing return
-			int avg = 0;
-			//variable to hold the sum of all the ascii values
-			double sum = 0.0;
-			//variable to hold the double division of the sum 4 to be able to cast ceiling as an int
-			double average = 0.0;
-			for(int i = 0; i < mesoStation.getStID().length(); i++ )
-			{
-				sum += mesoStation.getStID().charAt(i);
-			}
-			
-			//getting the raw double ascii average
-			average = sum / 4;
-			
-			//math operation to get just the fraction of the average
-			double fraction = average % 1;
-			
-			if(fraction >= 0.5)
-			{
-				//performing ceiling rounding on the ascii average and casting to int and assigning it to ceiling
-				avg = (int)Math.ceil(average);
-			}
-			else
-			{
-				avg = (int)Math.floor(average);
-			}
-			
-				
-			return avg;  
+			sum += mesoStation.getStID().charAt(i);
 		}
+
+		// getting the raw double ascii average
+		average = sum / mesoStation.getStID().length();
+
+		// math operation to get just the fraction of the average
+		double fraction = average % 1;
+
+		if (fraction >= 0.5) 
+		{
+			// performing ceiling division on the ascii average and casting to int and
+			// assigning it to ceiling
+			avg = (int) Math.ceil(average);
+		} 
+		else 
+		{
+			// performing floor division on the ascii average and casting to int and
+			// assigning it to ceiling
+			avg = (int) Math.floor(average);
+		}
+
+		return avg;
+	}
+
 }
