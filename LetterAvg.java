d22e8e3a0b6393897899495a076cd10742d955be
Zeo G <zeog14@gmail.com>
2019-10-10 12:53:21 -0500

    Implemented readStations() in the Contstructor

--- LetterAvg.java
+++ LetterAvg.java
@@ -7,23 +7,34 @@ public class LetterAvg extends MesoInherit
 	//char for first letter
 	private char firstLetter;
 	
-	//array of strings to hold the names of the stations
+	/**
+	 * ArrayList that holds the names of the stations that begin with the specified char
+	 */
 	private ArrayList<String> nameOfStations = new ArrayList<String>();
 	
-	//default constructor set initial values to 0;
+	/**
+	 * 
+	 * @param firstLetter recieves the letter that will be compared in numberOfStationWithLetterAvg() to find the stations that begin with the letter
+	 * @throws IOException
+	 */
 	public LetterAvg(char firstLetter) throws IOException
-	{
+	{	
+		readStations();
 		this.firstLetter = firstLetter;
 	}
 	
 	
 
-	//from driver
+	/**
+	 * This method compares compares the first letter of the stations in the array list to firstLetter. 
+	 * It also adds the stations that do begin with the letter. 
+	 * @return an int containing the amount of stations that start with firstLetter.
+	 */
 	public int numberOfStationWithLetterAvg()
 	{
 		int numStations = 0;
 		char[] toCompare = new char[0];
-		String station;
+		String station; 
 		for(int i = 0 ; i < stations.size(); i++ )
 		{
 			toCompare = stations.get(i).toCharArray();
@@ -34,18 +45,30 @@ public class LetterAvg extends MesoInherit
 				numStations++;
 			}
 		}
-		
 		return numStations; 
 	}
 	
 	
-	//will need to return formatted string (From driver)
+	
+	/**
+	 * Return the class information in the proper format
+	 */
 	public String toString()
 	{
-			String toPrint  = String.format("They are: \n");
+		 
+			
+			String toPrint  = String.format("\nThey are:\n");
 			for(int i = 0; i < nameOfStations.size(); i++)
 			{
-				toPrint += nameOfStations.get(i) + "\n";
+				if(i < (nameOfStations.size() - 1)) 
+				{
+					toPrint += nameOfStations.get(i) + "\n";
+				}
+				else
+				{
+					toPrint+= nameOfStations.get(i);
+				}
+				
 			}
 			
 			return toPrint;
